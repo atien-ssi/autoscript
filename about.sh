0ae82a5647f4b188b8f0c0866f669bad538d3153
suratin@datasys-it.co.id <suratin@datasys-it.co.id>
2021-09-30 13:38:06 +0700

    init

---
+++ about.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "# Script Auto Install SSH & OpenVPN             #"
+echo -e "#-----------------------------------------------#"
+echo -e "# For Debian 9 & 10 Buster 64 bit               #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "# Modified by Karlos Geek                       #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Thanks To                                     #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Karlos Geek                                   #"
+echo -e "# My Family                                     #"
+echo -e "# Google                                        #"
+echo -e "# REDESGEEK.COM                                 #"
+echo -e "#                                               #"
+echo -e "#-----------------------------------------------#"
+echo -e "================================================="
